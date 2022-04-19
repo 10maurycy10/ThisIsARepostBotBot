2f829bb5380a3cbd2dd7e1571d57ea3add323bef
Maurycy Z <10maurycy10@gmail.com>
2022-04-19 13:48:13 -0700

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# ThisIsARepostBotBot
+
+> So you found a repost bot, now what?
+
+This is a bot to reply to all posts of a repost bot with a message urging users to report and down vote, It can also report posts. (You may want a burner account for this.)
+
+## Requirements
+
+### reddit
+
+A reddit account with a added script.
+
+To add a script to your reddit account, go to https://old.reddit.com/prefs/apps, click "create (another) app", select "personal use script", and enter "http://localhost/" as the redirect uri.
+
+(Keep the page open, you will need the client secret and client id values to configure the script)
+
+### pip
+
+- praw
+
+- mariadb
+
+### DB
+
+This uses an MySQL/mariadb database to store lists of bot users and keep track of reported posts.
+
+You can create the required table structure like this:
+
+```sql
+create database repost
+use repost
+create table bots (username: varchar(60))
+create table reported_posts (id: varchar(10))
+```
+
+## Configuration
+
+The main configuration file is "config.json", a template file is provided, "config.json.exmple".
+
+## Running
+
+just ``python3 bot.py``
---
+++ bot.py
@@ -0,0 +1,64 @@
+import praw
+import json
+import mariadb
+
+config = {}
+with open("config.json", "r") as conffile:
+    config = json.loads(conffile.read())
+
+print("Connecting to DB")
+
+db = mariadb.connect(
+    user=config["db_username"],
+    password=config["db_passwd"],
+    host=config["db_host"],
+    port=config["db_port"],
+    database=config["db_dbname"]
+)
+
+print("Feching bot list")
+
+dbc = db.cursor()
+dbc.execute("select username from bots")
+bot_list = [i[0] for i in dbc]
+
+print("Feching list of reported posts")
+
+dbc = db.cursor()
+dbc.execute("select id from reported_posts")
+flaged_posts = [i[0] for i in dbc]
+
+print("Connecting to reddit")
+
+r = praw.Reddit(client_id=config['reddit_client_id'], client_secret=config['reddit_client_secret'], username=config["reddit_username"], password=config["reddit_passwd"], user_agent=config["reddit_ua"])
+
+message = "The poster of the post/comment I am replying to is a karma farming repost bot, please downvote and report.\n\nIf you want to know how I know this, PM me."
+
+#bot_list = ["Jazminedg","Trinitysjm","Luna_Mcmahan346","Sariah_jb","Sylviafx","Nuria_Vega54"]
+
+is_post_flaged = {}
+
+for post in flaged_posts:
+    is_post_flaged[post] = True
+
+print(is_post_flaged)
+
+for bot in bot_list:
+    bot_handle = r.redditor(bot);
+    print("Reporting " + bot_handle.name)
+    for post in bot_handle.submissions.new():
+        if not(str(post) in is_post_flaged):
+            print("Flaging : " + str(post))
+            if config["repost_reply"] is not None:
+                post.reply(config["repost_reply"]);
+            if config["repost_report"]:
+                post.report("Spam")
+            dbc = db.cursor()
+            dbc.execute("insert into reported_posts (id) values (?)",  (str(post),))
+            print("Shortlink : " + post.shortlink)
+            is_post_flaged[str(post)] = True
+        else:
+            print("Skiping " + str(post));
+
+db.commit() 
+db.close()
